78a5244330553cf4208f40bbb2a2d2786d07cd30
faiqua-fatima <ff382@live.mdx.ac.uk>
2025-02-25 10:32:53 +0400

    modify the print statement

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hiiiiiiiii, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("Hii FAiqua")
-
+print("2+2 is not equal to 4")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
